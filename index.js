7454a0b16b0add474c8262db8d863040d35737d8
Duncan Kipkemoi <kipkemoiduncan7@gmail.com>
2023-03-16 15:52:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const returnFirstTwoDrivers = (drivers)=>
+    ([drivers[0] , drivers[1]])
+
+
+
+const returnLastTwoDrivers = (drivers)=>
+    ([drivers[2] , drivers[3]])
+
+const selectingDrivers=[returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(drive){
+    return function (amount){
+        return drive*amount;
+    }
+}
+const fareDoubler= createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers=(arrayOfDrivers, anyDriver)=>anyDriver(arrayOfDrivers);
+
+
